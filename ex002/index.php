89bd924147d5e9f124896d841caa44277005b114
Mario2024-Lemene <177216495+Mario2024-Lemene@users.noreply.github.com>
2024-11-13 14:52:03 +0200

    Testando funcao em PHP

--- ex002/index.php
+++ ex002/index.php
@@ -3,9 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Php</title>
+    <title>PHP</title>
 </head>
 <body>
-    
+    <h1>Exemplo de PHP</h1>
+    <?php 
+        echo "Hoje é  dia ". date("d/M/Y");
+    ?>
 </body>
 </html>
